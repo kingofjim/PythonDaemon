61541128455f38ba07ef05b26257f7982cbeeebc
Jim Chang <bill3975598@gmail.com>
2020-08-27 19:31:03 +0800

    update ELK query with new nginx_timestamp

--- elasticsearch.py
+++ elasticsearch.py
@@ -13,14 +13,13 @@ class Elasticsearch:
         # self.body['json'] = '{"track_total_hits": true}'
 
     def search_sendbtye_by_domains(self, period):
-
-
-        body = '{"size":0,"query":{"constant_score":{"filter":{"range":{"@timestamp":{"gte":"'+period[0]+'","lt":"'+period[1]+'"}}}}},"aggs":{"domains":{"terms":{"field":"request_host.keyword","size": 999999999},"aggs":{"body_bytes_sent":{"sum":{"field":"body_bytes_sent"}}}}}}'
-        write_app_log("Main - Elasticsearch: %s\n" % body)
+        # body = '{"size":0,"query":{"constant_score":{"filter":{"range":{"@timestamp":{"gte":"'+period[0]+'","lt":"'+period[1]+'"}}}}},"aggs":{"domains":{"terms":{"field":"request_host.keyword","size": 999999999},"aggs":{"body_bytes_sent":{"sum":{"field":"body_bytes_sent"}}}}}}'
+        body = '{"size":0,"query":{"constant_score":{"filter":{"range":{"nginx_timestamp":{"gte":"%s","lte":"%s","format":"strict_date_optional_time"}}}}},"aggs":{"domains":{"terms":{"field":"request_host.keyword","size":999999999},"aggs":{"body_bytes_sent":{"sum":{"field":"body_bytes_sent"}}}}}}' % (period[0], period[1])
+        print("[Web-Main] search_sendbtye_by_domains - body: %s" % body)
         # exit()
         response = requests.get('http://35.201.180.3:9200/logstash-hqs-cdn-proxy-*/_search', auth=self.credentials, headers=self.headers, data=body)
         if(response.status_code == 200):
-            print(response.text)
+            # print(response.text)
             response = response.json()
             shards = response['_shards']
             if shards['total'] != shards['total']:
@@ -36,8 +35,9 @@ class Elasticsearch:
             raise Exception('Elasticsearch search_sendbtye_by_domains not respond 200\n')
 
     def search_city_count_distribution(self, period):
-        body = '{"size":0,"query":{"constant_score":{"filter":{"range":{"@timestamp":{"gte":"'+period[0]+'","lt":"'+period[1]+'"}}}}},"aggs":{"domains":{"terms":{"field":"request_host.keyword","size": 999999999},"aggs":{"country":{"terms":{"field":"geoip.country_name.keyword"},"aggs":{"distribution":{"terms":{"size":999999999,"field":"geoip.region_name.keyword"}}}}}}}}'
-        write_app_log("Side - Elasticsearch city: %s\n" % body)
+        # body = '{"size":0,"query":{"constant_score":{"filter":{"range":{"@timestamp":{"gte":"'+period[0]+'","lt":"'+period[1]+'"}}}}},"aggs":{"domains":{"terms":{"field":"request_host.keyword","size": 999999999},"aggs":{"country":{"terms":{"field":"geoip.country_name.keyword"},"aggs":{"distribution":{"terms":{"size":999999999,"field":"geoip.region_name.keyword"}}}}}}}}'
+        body = '{"size":0,"query":{"constant_score":{"filter":{"range":{"nginx_timestamp":{"gte":"%s","lte":"%s","format":"strict_date_optional_time"}}}}},"aggs":{"domains":{"terms":{"field":"request_host.keyword","size":999999999},"aggs":{"country":{"terms":{"field":"geoip.country_name.keyword"},"aggs":{"distribution":{"terms":{"size":999999999,"field":"geoip.region_name.keyword"}}}}}}}}' % (period[0], period[1])
+        write_app_log("[Web] search_city_count_distribution - body: %s\n" % body)
         response = requests.get('http://35.201.180.3:9200/logstash-hqs-cdn-proxy-*/_search', auth=self.credentials, headers=self.headers, data=body)
         if(response.status_code == 200):
             response = response.json()
@@ -59,8 +59,9 @@ class Elasticsearch:
             raise Exception('Elasticsearch search_city_count_distribution not respond 200\n')
 
     def search_status_distribution(self, period):
-        body = '{"size":0,"query":{"constant_score":{"filter":{"range":{"@timestamp":{"gte":"' + period[0] + '","lt":"' + period[1] + '"}}}}},"aggs":{"domains":{"terms":{"field":"request_host.keyword","size":999999999},"aggs":{"status":{"terms":{"size":999999999,"field":"status.keyword"}}}}}}'
-        write_app_log("Side - Elasticsearch status: %s\n" % body)
+        # body = '{"size":0,"query":{"constant_score":{"filter":{"range":{"@timestamp":{"gte":"' + period[0] + '","lt":"' + period[1] + '"}}}}},"aggs":{"domains":{"terms":{"field":"request_host.keyword","size":999999999},"aggs":{"status":{"terms":{"size":999999999,"field":"status.keyword"}}}}}}'
+        body = '{"size":0,"query":{"constant_score":{"filter":{"range":{"nginx_timestamp":{"gte":"%s","lte":"%s","format":"strict_date_optional_time"}}}}},"aggs":{"domains":{"terms":{"field":"request_host.keyword","size":999999999},"aggs":{"status":{"terms":{"size":999999999,"field":"status.keyword"}}}}}}' % (period[0], period[1])
+        print("[Web] - Elasticsearch status - body: %s" % body)
         # print(body)
         # exit()
         response = requests.get('http://35.201.180.3:9200/logstash-hqs-cdn-proxy-*/_search', auth=self.credentials, headers=self.headers, data=body)
@@ -88,7 +89,7 @@ class Elasticsearch:
     def search_dns_query_by_domains(self, period):
 
         body = '{"aggs":{"2":{"terms":{"field":"query_value.keyword","order":{"_count":"desc"},"size":9999999}}},"size":0,"_source":{"excludes":[]},"stored_fields":["*"],"script_fields":{},"docvalue_fields":[{"field":"@timestamp","format":"date_time"}],"query":{"bool":{"must":[],"filter":[{"match_all":{}},{"range":{"@timestamp":{"format":"strict_date_optional_time","gte":"%s","lt":"%s"}}}],"should":[],"must_not":[]}}}' % (period[0], period[1])
-        write_app_log("Main - Elasticsearch: %s\n" % body)
+        print("[DNS] search_dns_query_by_domains - body: %s" % body)
         # exit()
         response = requests.get('http://35.201.180.3:9200/logstash-hqs-cdn-dns-*/_search/', auth=self.credentials, headers=self.headers, data=body)
         if response.status_code == 200:
@@ -110,7 +111,7 @@ class Elasticsearch:
 
     def search_dns_query_by_ip(self, period):
         body = '{"aggs":{"2":{"terms":{"field":"client_ip.keyword","order":{"_count":"desc"},"size":9999999,"min_doc_count":100},"aggs":{"3":{"terms":{"field":"query_value.keyword","order":{"_count":"desc"},"size":999999}}}}},"size":0,"_source":{"excludes":[]},"stored_fields":["*"],"script_fields":{},"docvalue_fields":[{"field":"@timestamp","format":"date_time"}],"query":{"bool":{"must":[],"filter":[{"match_all":{}},{"range":{"@timestamp":{"format":"strict_date_optional_time","gte":"%s","lt":"%s"}}}],"should":[],"must_not":[]}}}' % (period[0], period[1])
-        write_app_log("Main - Elasticsearch: %s\n" % body)
+        print("[DNS] search_dns_query_by_ip - body: %s" % body)
         # exit()
         response = requests.get('http://35.201.180.3:9200/logstash-hqs-cdn-dns-*/_search/', auth=self.credentials, headers=self.headers, data=body)
         if response.status_code == 200:
@@ -121,7 +122,7 @@ class Elasticsearch:
                 write_error_log('ERROR!!! %s ~ %s DNS-IP job shards failed\n' % (period[0], period[1]))
                 raise Exception('ERROR!!! %s ~ %s DNS-IP job shards failed' % (period[0], period[1]))
             response_bucket = response['aggregations']['2']['buckets']
-            print(response_bucket)
+            # print(response_bucket)
 
             if response_bucket:
                 result = {}
